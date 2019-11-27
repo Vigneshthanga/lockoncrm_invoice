510763e73f6ed8bdd0f371cc8e78941cccf2c0ff
Vigneshthanga <vigneshkumar.thangarajan@gmail.com>
2019-11-26 17:47:18 -0800

    adding docker required file

---
+++ boot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+# start service discovery task in the background
+if [ "$SERVICE_URL" != "" ]; then
+    python -c "from lockoncrm_common.container import register; register()" &
+fi
+
+# run web server
+exec gunicorn -b 0.0.0.0:5003 --log-level=debug --access-logfile - --error-logfile - app:app --workers 3 --timeout 120
